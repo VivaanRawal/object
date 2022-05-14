de240aeb74f124b82f9b3ee00ade97b26e526f7f
VivaanRawal <80296406+VivaanRawal@users.noreply.github.com>
2022-05-13 17:00:17 -0700

    Add files via upload

---
+++ object-detection2/main copy.js
@@ -0,0 +1,17 @@
+img="";
+function preload(){
+img=loadImage('TV.jpeg');
+
+}
+function setup(){
+    canvas=createCanvas(640,420);
+    canvas.center();
+}
+function draw(){
+    image(img,0,0,640,420);
+    fill("red");
+    text("TV",45,75);
+    noFill();
+    stroke("red");
+    rect(30,60,450,350);
+}
